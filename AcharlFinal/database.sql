17ab508de04abd397a82c9b078cbaa3460c858a2
Charlo sicuya <46435429+charlosicuya90@users.noreply.github.com>
2019-11-25 23:04:07 -0800

    Add files via upload

---
+++ AcharlFinal/database.sql
@@ -0,0 +1,11 @@
+create database Acharl;
+
+use Acharl;
+
+CREATE TABLE `Acharl` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) NOT NULL,
+  `age` int(3) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY  (`id`)
+);
